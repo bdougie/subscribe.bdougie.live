812e0fa776386668d8294e7c398ffd45ab627669
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-08-23 18:01:53 -0700

    fix email handling form.

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,7 +3,32 @@ const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
 exports.handler = async event => {
   const email = JSON.parse(event.body).payload.email
-  console.log(`Recieved a submission: ${email}`)
+  console.log(`Received a submission: ${email}`)
+
+  const processForm = form => {
+    const data = new FormData(form)
+    data.append('form-name', 'newsletter');
+    fetch('/', {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+    })
+    .catch(error => {
+      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+    })
+  }
+
+  const emailForm = document.querySelector('.email-form')
+
+  if (emailForm) {
+    emailForm.addEventListener('submit', e => {
+      e.preventDefault();
+      processForm(emailForm);
+    })
+  }
+
   return fetch('https://api.buttondown.email/v1/subscribers', {
     method: 'POST',
     headers: {
@@ -18,3 +43,4 @@ exports.handler = async event => {
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
 }
+
